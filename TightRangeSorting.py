312985d1a7cd9fc2fe573b76aff55c7615c8e994
akshatnahata <akshatnahata05@gmail.com>
2022-07-12 03:37:00 +0530

    first commit

---
+++ TightRangeSorting.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import os
+import glob
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+dict = {}
+stocks = pd.read_csv('stocklist.csv')
+df = pd.DataFrame(stocks)
+df = df.iloc[: , 1:]
+arr = df["0"].to_numpy()
+
+# print(arr)
+for stock in arr:
+
+    df = pd.read_csv(f'filesForToday/{stock}.csv')
+    df = pd.DataFrame(df)
+    
+    minhigh = df.iloc[0]['h']
+    minlow = df.iloc[0]['l']
+    rangee = (minhigh-minlow)/minhigh*100
+    stock = stock+","
+    dict[stock] = rangee
+
+a = sorted(dict.items(), key=lambda x: x[1])
+# print(a) 
+a = pd.DataFrame(a)
+a.to_csv('stockToTrade.csv')  
